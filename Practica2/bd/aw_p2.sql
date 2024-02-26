eadd08e7542d7c48b66d0082697b12db89701513
Gabrif04 <117313158+Gabrif04@users.noreply.github.com>
2024-02-26 12:22:57 +0100

    bd2

--- Practica2/bd/aw_p2.sql
+++ Practica2/bd/aw_p2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 26-02-2024 a las 12:08:11
+-- Tiempo de generación: 26-02-2024 a las 12:22:06
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -30,11 +30,38 @@ SET time_zone = "+00:00";
 CREATE TABLE `productos` (
   `Nombre` varchar(30) NOT NULL,
   `Descripción` text NOT NULL,
-  `Precio` decimal(6,2) NOT NULL,
+  `Precio` decimal(4,0) NOT NULL,
   `Categoría` varchar(15) NOT NULL,
   `Existencias` int(2) NOT NULL,
   `Especie` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `Nombre` varchar(20) NOT NULL,
+  `Apellidos` varchar(50) NOT NULL,
+  `Correo` varchar(50) NOT NULL,
+  `Dirección` varchar(100) NOT NULL,
+  `Teléfono` int(9) NOT NULL,
+  `DNI` varchar(9) NOT NULL,
+  `Usuario` varchar(20) NOT NULL,
+  `Contraseña` varchar(20) NOT NULL,
+  `Tipo` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`Nombre`, `Apellidos`, `Correo`, `Dirección`, `Teléfono`, `DNI`, `Usuario`, `Contraseña`, `Tipo`) VALUES
+('Admin', 'admin', 'admin@ucm.es', 'calle ejemplo nº1 2ºA', 666777888, '12345678O', 'admin', 'adminpass', 1),
+('Álvaro', 'González-Barros Medina', 'barrosloveszaka@gmail.com', 'Calle ejemplo nº1, 2ºS', 675456623, '45633891B', 'barros', '1234', 1),
+('User', 'user', 'user@ucm.es', 'calle ejemplo nº3 2ºB', 666111888, '87654321Q', 'user', 'userpass', 0);
 
 --
 -- Índices para tablas volcadas
@@ -45,6 +72,12 @@ CREATE TABLE `productos` (
 --
 ALTER TABLE `productos`
   ADD PRIMARY KEY (`Especie`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`DNI`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
