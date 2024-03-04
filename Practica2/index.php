c7d554e22205fbd64fa01916f17698b83f3768a7
alexlopez2303 <alejlo24@ucm.es>
2024-03-04 12:36:42 +0100

    .

--- Practica2/index.php
+++ Practica2/index.php
@@ -6,7 +6,7 @@ $tituloPagina = 'Portada';
 
 $contenidoPrincipal=<<<EOS
 <h1>Página principal</h1>
-	<p> Aquí está el contenido público, visible para todos los usuarios. </p>
+	<p> Aquí está el contenido público, visible para todos los usuarios.</p>
 EOS;
 
 require 'includes/vistas/comun/layout.php';
