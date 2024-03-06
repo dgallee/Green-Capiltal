7a59e705c0f30f0fc1cc889bb81c0fb67af6acb3
Gabrif04 <117313158+Gabrif04@users.noreply.github.com>
2024-03-06 10:32:29 +0100

    bd

--- Practica2/includes/mysql/datos.sql
+++ Practica2/includes/mysql/datos.sql
@@ -1,9 +1,7 @@
-/*
-  Recuerda que deshabilitar la opción "Enable foreign key checks" para evitar problemas a la hora de importar el script.
-*/
-TRUNCATE TABLE `usuarios`;
+--
+-- Volcado de datos para la tabla `usuarios`
+--
 
-INSERT INTO `usuarios` (`Nombre`, `Apellidos`, `Correo`, `Dirección`, `Teléfono`, `DNI`, `Usuario`, `Contraseña`, `Tipo`) VALUES
-('Admin', 'admin', 'admin@ucm.es', 'calle ejemplo nº1 2ºA', 666777888, '12345678O', 'admin', 'adminpass', 1),
-('Álvaro', 'González-Barros Medina', 'barrosloveszaka@gmail.com', 'Calle ejemplo nº1, 2ºS', 675456623, '45633891B', 'barros', '1234', 1),
+INSERT INTO `usuarios` (`Nombre`, `Apellidos`, `Correo`, `Direccion`, `Telefono`, `DNI`, `Usuario`, `Contrasena`, `Tipo`) VALUES
+('Admin', 'Prueba', 'admin@ucm.es', 'calle ejemplo nº1 2ºA', 666555111, '12345678O', 'admin', 'adminpass', 1),
 ('User', 'user', 'user@ucm.es', 'calle ejemplo nº3 2ºB', 666111888, '87654321Q', 'user', 'userpass', 0);
