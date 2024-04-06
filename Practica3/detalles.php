09aa61ca70e816fa64580a26b6a20719195d1cb4
alexlopez2303 <alejlo24@ucm.es>
2024-04-06 21:35:42 +0200

    implementado las unidades a comprar de un producto (javascript)

--- Practica3/detalles.php
+++ Practica3/detalles.php
@@ -1,22 +1,34 @@
 <?php
+require_once "includes/src/Aplicacion.php";
+require_once "includes/config.php";
+require_once 'includes/vistas/helpers/usuarios.php';
+require_once 'productosDAO.php';
+require_once 'includes/vistas/helpers/detalles.php';
 
-    require_once "includes/src/Aplicacion.php";
-    require_once "includes/config.php";
-    require_once 'includes/vistas/helpers/usuarios.php';
-    require_once 'productosDAO.php';
-    require_once 'includes/vistas/helpers/detalles.php';
-    
-    $tituloPagina = "Detalles del producto";
+$tituloPagina = "Detalles del producto";
 
-    $id = $_GET['prod'];
-    $caracteristicas = Producto::search($id);
-    $detallesProd = builtDetails($caracteristicas->getNombre(), $caracteristicas->getId(), $caracteristicas->getDesc(), $caracteristicas->getPrecio(), $caracteristicas->getCategoria(), $caracteristicas->getExistencias(), $caracteristicas->getEspecie(), $caracteristicas->getImagen());
+$id = $_GET['prod'];
+$caracteristicas = Producto::search($id);
+$cantidad = isset($_POST['cantidad']) ? max(1, intval($_POST['cantidad'])) : 1;
+$existencias = $caracteristicas->getExistencias();
 
-    $contenidoPrincipal = <<<EOS
-    <p>$detallesProd</p>
-    EOS;
-   
+$detallesProd = builtDetails($caracteristicas->getNombre(), $caracteristicas->getId(), $caracteristicas->getDesc(), $caracteristicas->getPrecio(), $caracteristicas->getCategoria(), $existencias, $caracteristicas->getEspecie(), $caracteristicas->getImagen(), $cantidad);
 
+$contenidoPrincipal = <<<EOS
+<p>$detallesProd</p>
+<form id="form-cantidad">
+    <p>Unidades a comprar: <span id="unidades-comprar">$cantidad</span></p>
+    <input type="hidden" id="cantidad" name="cantidad" value="$cantidad">
+    <button type="button" id="btn-sumar">+</button>
+    <button type="button" id="btn-restar">-</button>
+</form>
+EOS;
 
-    require('includes/vistas/comun/layout.php');
+require('includes/vistas/comun/layout.php');
 ?>
+<script>
+    var existencias = <?php echo $existencias; ?>;
+</script>
+<script src="js/compra.js"></script>
+
+
