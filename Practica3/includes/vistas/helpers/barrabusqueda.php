63f30c1c1c6e395e0186dc20bf74ee76159edfac
dgallee <dangal04@ucm.es>
2024-04-08 11:16:21 +0200

    barra busqueda fin

--- Practica3/includes/vistas/helpers/barrabusqueda.php
+++ Practica3/includes/vistas/helpers/barrabusqueda.php
@@ -21,7 +21,7 @@ function buildFormularioBusqueda($itemname = '', $itemCat = '') {
     $categorias = getCategorias();
 
     $ret = <<<EOS
-    <div class="contenedor de busqueda">
+    <div class="contenedor-de-busqueda">
         <form method="get" action="$ruta" class="search-form">
             <div class="search-container">
                 <input type="text" id="search-input" class="search-input" name="busqueda" placeholder="$itemname">
@@ -32,23 +32,26 @@ function buildFormularioBusqueda($itemname = '', $itemCat = '') {
                 <label for="filter1">Price:</label>
                 <span id="filter1-value">$0</span>
                 <input type="range" id="filter1" name="filter1" min="0" max="100" step="0,1" value="100">  
-                <span id="filter1-max">$100</span>
+                <span id="filter1-max">100</span>
                 
-                <div class="filter-item">
-                    <label for="filter2">Filter by category:</label>
-                    <select id="filter2" name="filter2" placeholder="$itemCat">
-                        <option value="">--Select category--</option>
+                
+
+                <label for="filter2">Filter by category:</label>
+                <select id="filter2" name="filter2" placeholder="$itemCat">
+                <option value="">--Select category--</option>
 EOS;
-                        foreach ($categorias as $categoria) {
-                            $ret .= <<<EOS
-                            <option value="$categoria">$categoria</option>
-EOS;
-                        }
+                    foreach ($categorias as $categoria) {
                         $ret .= <<<EOS
-                    </select>
-                </div>
-                
-                <button type="submit">Search</button>
+                        <option value="$categoria">$categoria</option>
+EOS;
+                    }
+                    $ret .= <<<EOS
+                </select>
+                 
+            </div>
+            
+            <div class="filter-butons">
+                <button type="submit" class="search-button">Search</button>
                 <div class="filter-item clear-all-button" id="clear-all-filters">Clear All</div>
             </div>
         </form>
--- Practica3/procesarBusqueda.php
+++ Practica3/procesarBusqueda.php
@@ -14,15 +14,55 @@ $conn = new mysqli(BD_HOST,BD_USER,BD_PASS,BD_NAME);
 
 $termino = isset($_GET["busqueda"]) ? trim($_GET["busqueda"]) : "";
 
+if (isset($_GET["filter2"]) && $_GET["filter2"] != "") {
+    $categoria = $_GET["filter2"];
+    
+}
+
+if (isset($_GET["filter1"])) {
+    $precio = $_GET["filter1"];
+    // Ajusta esta parte según tu estructura de precios en la base de datos
+  
+}
 
 
 
+$query = "SELECT * FROM productos WHERE 1";
 // buscar los items que contengan el término ingresado
 if (!empty($termino)) {
-    $query = "SELECT * FROM productos WHERE Nombre LIKE ? OR Descripcion LIKE ?";
+    
+    
+    $query .= " AND (Nombre LIKE ? OR Descripcion LIKE ?)";
+
+    if (!empty($categoria)) {
+        
+        $query .= " AND Categoria = ?";
+    }
+    
+    if (!empty($precio)) {
+       
+        $query .= " AND Precio <= ?";
+    }
+    
     $statement = $conn->prepare($query);
     $termino = "%".$termino."%";
-    $statement->bind_param("ss", $termino, $termino);
+    if(!empty($categoria)  and !empty($precio)){
+        $statement->bind_param("sssd", $termino, $termino,$categoria, $precio);
+        
+    }
+    else if(!empty($categoria)){
+        $statement->bind_param("sss", $termino, $termino, $categoria);
+    }
+    else if(!empty($precio)){
+        $statement->bind_param("ssd", $termino, $termino, $precio);
+    }
+    else{
+        $statement->bind_param("ss", $termino, $termino);
+    }
+
+    echo "Query: ". $query; // Add this line to print the query
+
+    
     $statement->execute();
     $items = $statement->get_result()->fetch_all(MYSQLI_ASSOC);
 
