7d20880550aca1dbdf097f2ca475232e51c46c6e
Zack1223 <117909834+Zack1223@users.noreply.github.com>
2024-04-17 10:49:33 +0200

    Add files via upload

--- Practica3/includes/vistas/comun/busqueda.php
+++ Practica3/includes/vistas/comun/busqueda.php
@@ -3,5 +3,5 @@ require_once __DIR__.'/../helpers/barrabusqueda.php';
 ?>
 
 <div class="busqueda">
-        <p><?= buildFormularioBusqueda()?></p>
+        <p><?= buildFormularioBusqueda()?>
 </div>
