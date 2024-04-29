8598a83c080e942b8c477746b233b04186df3b99
Zack1223 <117909834+Zack1223@users.noreply.github.com>
2024-04-29 11:40:17 +0200

    Javascript corregido

--- Practica3/js/valoraciones.js
+++ Practica3/js/valoraciones.js
@@ -5,6 +5,7 @@
 document.addEventListener("DOMContentLoaded", function() {
   let boton = document.getElementById("miBoton");
   let eliminar = document.getElementById("eliminarvaloracion");
+  let botones= document.getElementsByClassName("botonvalorar");
   
  
   
@@ -14,6 +15,16 @@ document.addEventListener("DOMContentLoaded", function() {
     
     location.href = "valoraciones.php?" + "Dni=" + this.getAttribute("dni") + "&idProd=" + this.getAttribute("idProducto");
   });
+  if(botones!=null){
+
+
+    for(let i=0;i<botones.length;i++)
+  botones[i].addEventListener("click", function() {
+    
+    location.href = "valoraciones.php?" + "Dni=" + this.getAttribute("dni") + "&idProd=" + this.getAttribute("idProducto");
+  });
+
+}
 
   if(eliminar!=null)
   eliminar.addEventListener("click", function() {
