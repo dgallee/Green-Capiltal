987414cb691873ed2be455cea3b9ae0ba8b3ec17
Zack1223 <117909834+Zack1223@users.noreply.github.com>
2024-04-29 09:21:35 +0200

    Formulario Valoracion

--- Practica3/includes/src/FORMULARIOAgregarValoracion.php
+++ Practica3/includes/src/FORMULARIOAgregarValoracion.php
@@ -5,8 +5,10 @@ require_once 'includes/src/Formulario.php';
 require_once 'includes/config.php';
 require_once 'includes/vistas/helpers/autorizacion.php';
 require_once 'productosDAO.php';
+require_once 'valoracionesDAO.php';
 
 use Producto;
+use Valoracion;
 
 class FormularioAgregarValoracion extends Formulario {
     private $Valoracion;
@@ -29,6 +31,17 @@ class FormularioAgregarValoracion extends Formulario {
         $erroresCampos = self::generaErroresCampos(['Valoracion'], $this->errores, 'span', array('class' => 'error'));
 
         $tipo=0;
+        $boton='';
+        if($this->Valoracion!=''){
+
+
+            $boton=<<<EOS
+            <button type ="button" id='eliminarvaloracion' dni='{$_SESSION['DNI']}' idProducto='{$this->idProd}' >Eliminar </button>'
+            
+            EOS;
+
+        }
+    
 
         $html = <<<EOS
         $htmlErroresGlobales
@@ -38,15 +51,15 @@ class FormularioAgregarValoracion extends Formulario {
             <label for="Producto">Producto:</label>
             <input type="text" name="Producto" id="Producto" value="$this->idProd" readonly>
         </div>
+        <input type="hidden" name="dni" value="$this->Dni">
 
         <div>
             <label for="Valoracion">Valoracion:</label>
-            <input type="text" name="Valoracion" id="Valoracion" required>
+            <input type="text" name="Valoracion" id="Valoracion" value="$this->Valoracion"  required>
         </div>
         <div>
-        <label for="type">Puntuacion:</label>
-        <select name="type" id="type">
-            <option value="0" . ($tipo == 0 ? 'selected' : '') . >0</option>
+        <label for="point">Puntuacion:</label>
+        <select name="point" id="point">
             <option value="1" . ($tipo == 1 ? 'selected' : '') . >1</option>
             <option value="2" . ($tipo == 2 ? 'selected' : '') . >2</option>
             <option value="3" . ($tipo == 3 ? 'selected' : '') . >3</option>
@@ -55,7 +68,11 @@ class FormularioAgregarValoracion extends Formulario {
         </select>
         </div>
         
-        <button type="submit">Ingresar</button>
+        <button type="submit">Confirmar</button>
+        <br>
+
+        $boton
+        
         </fieldset>
         </form>
         EOS;
@@ -66,7 +83,17 @@ class FormularioAgregarValoracion extends Formulario {
 
     protected function procesaFormulario(&$datos)
     {
+
         
+        //Valoracion::addValoracion($datos['Producto'],$datos['dni'],$datos['point'],$datos['Valoracion']);
+
+        if($this->Valoracion==''){
+        Valoracion::addValoracion($datos['dni'],$datos['Producto'],$datos['point'],$datos['Valoracion']);
+        }else{
+
+        Valoracion::editValoracion($datos['dni'],$datos['Producto'],$datos['point'],$datos['Valoracion']);
+
+        }
 
 
 
