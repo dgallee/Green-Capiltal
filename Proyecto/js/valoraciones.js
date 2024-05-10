a8f37179a829b885884f752d2a084ab84884e3bb
Zack1223 <117909834+Zack1223@users.noreply.github.com>
2024-05-10 18:40:53 +0200

    Add files via upload

--- Proyecto/js/valoraciones.js
+++ Proyecto/js/valoraciones.js
@@ -1,23 +1,32 @@
 document.addEventListener("DOMContentLoaded", function() {
-  let boton = document.getElementById("miBoton"); //detalles
+  let boton = document.getElementById("miBoton");
   let eliminar = document.getElementById("eliminarvaloracion");
-  let botones= document.getElementsByClassName("botonvalorar"); //pedidos
+  let botones= document.getElementsByClassName("botonvalorar");
   
+ 
+  
+
   if(boton!=null)
   boton.addEventListener("click", function() {
     
-    location.href = "valoraciones.php?" + "Dni=" + this.getAttribute("dni") + "&idProd=" + this.getAttribute("idProducto") + "&dir=0";
+    location.href = "valoraciones.php?" + "Dni=" + this.getAttribute("data-dni") + "&idProd=" + this.getAttribute("data-idProducto");
   });
   if(botones!=null){
+
+
     for(let i=0;i<botones.length;i++)
-      botones[i].addEventListener("click", function() {
-      location.href = "valoraciones.php?" + "Dni=" + this.getAttribute("dni") + "&idProd=" + this.getAttribute("idProducto") + "&dir=1";
+  botones[i].addEventListener("click", function() {
+    
+    location.href = "valoraciones.php?" + "Dni=" + this.getAttribute("data-dni") + "&idProd=" + this.getAttribute("data-idProducto");
   });
 
 }
+
   if(eliminar!=null)
   eliminar.addEventListener("click", function() {
-    location.href = "eliminarvaloracion.php?" + "Dni=" + this.getAttribute("dni") + "&idProd=" + this.getAttribute("idProducto");
+    location.href = "eliminarvaloracion.php?" + "Dni=" + this.getAttribute("data-dni") + "&idProd=" + this.getAttribute("data-idProducto");
   });
 
+
+ 
 });
